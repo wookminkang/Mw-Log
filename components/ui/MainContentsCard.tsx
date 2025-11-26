6f88cfd89cf9c4a6f26d15c812512072ab6b9bd8
kangminwook <kangmu950@gmail.com>
2025-11-27 00:01:38 +0900

    파비콘 + 레이아웃 변경

--- components/ui/MainContentsCard.tsx
+++ components/ui/MainContentsCard.tsx
@@ -1,5 +1,4 @@
 import { Card } from "@/components/ui/card"
-import { CaseSensitive } from "lucide-react"
 import { Separator } from "@/components/ui"
 import dayjs from "dayjs"
 import Image from "next/image"
@@ -31,40 +30,42 @@ function MainContentsCard({ data }: Partial<props>) {
   }
 
   return (
-    <div>
-      <Card className="w-full h-fit p-4 gap-4 cursor-pointer hover:bg-muted/15 transition-all duration-300">
-        <div className="flex items-start gap-4">
-          <div className="flex-1 min-w-0 overflow-hidden flex flex-col items-start gap-4">
+      <Card className="w-full h-fit p-4 gap-4 cursor-pointer hover:bg-muted/95 transition-all duration-200">
+        <div className="flex flex-col md:flex-row items-start gap-4">
+          <div className="flex-1 flex flex-col gap-3 w-full md:w-auto">
             {/* 썸네일과 제목 */}
-            <h3 className="text-base font-semibold tracking-tight line-clamp-1">
-              <CaseSensitive size={16} className="text-muted-foreground" />
-              <p className="text-xl">{data?.title}</p>
+            <h3 className="text-2xl font-semibold tracking-tight line-clamp-2">
+              {data?.title}
             </h3>
             {/* 본문 */}
-            <p className="line-clamp-3 text-muted-foreground break-words [overflow-wrap:anywhere]">
+            <p className="line-clamp-3 text-base text-muted-foreground ">
               {setContent(data?.content || "")}
             </p>
+            <p className="text-md text-muted-foreground">
+              {dayjs(data?.created_at).format('YYYY. MM. DD. HH:mm')}
+            </p>
           </div>
-          {data?.thumbnail ? (
-            <Image
-              src={data?.thumbnail}
-              alt="@THUMBNAIL"
-              width={140}
-              height={140}
-              className="w-[140px] h-[140px] aspect-square rounded-lg object-cover cursor-pointer"
-            />
-          ) : (
-            <div className="w-[140px] h-[140px] aspect-square rounded-lg object-cover bg-muted cursor-pointer"></div>
-          )}
+
+          <div className="flex-shrink-0 w-full md:w-auto">
+            {data?.thumbnail ? (
+              <Image
+                src={data?.thumbnail}
+                alt="@THUMBNAIL"
+                width={240}
+                height={140}
+                className="w-full md:w-[240px] h-auto md:h-[150px] aspect-video md:aspect-auto rounded-lg object-cover cursor-pointer"
+              />
+            ) : (
+              <div className="w-full md:w-[200px] h-auto md:h-[150px] aspect-video md:aspect-auto rounded-lg object-cover bg-muted cursor-pointer"></div>
+            )}
+          </div>
+          
         </div>
         <Separator />
         <div className="w-full flex items-center justify-between">
-          <p>글쓴이: 돌멩이</p>
-          <p>{dayjs(data?.created_at).format("YYYY. MM. DD.")}</p>
-          {/* <p>{dayjs(props.created_at).fromNow()}</p> */}
+          <p className="text-sm text-muted-foreground">글쓴이: 돌멩이</p>
         </div>
       </Card>
-    </div>
   )
 }
 
