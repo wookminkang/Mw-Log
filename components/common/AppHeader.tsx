a92fd6e77fe6af308b94dc4febb07e3c8eb3272d
mwkang1 <mwkang1@smartscore.kr>
2025-11-27 10:11:21 +0900

    Appheader orange 크기 변경

--- components/common/AppHeader.tsx
+++ components/common/AppHeader.tsx
@@ -1,17 +1,20 @@
-import Link from "next/link"
-import Image from "next/image"
+import Link from "next/link";
+import Image from "next/image";
 
 function AppHeader() {
   return (
     <header className="fixed top-0 z-10 w-full border-b bg-background/60 backdrop-blur supports-[backdrop-filter]:bg-background/40">
       <div className="mx-auto max-w-[1328px] px-4 py-3 flex items-center justify-between">
-        <Link href="/" className="text-base font-semibold flex items-center gap-2">
-          <Image src="/logo.png" alt="logo" width={20} height={20}  />
+        <Link
+          href="/"
+          className="text-base font-semibold flex items-center gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={18} height={18} />
           <span>MINWOOK</span>
         </Link>
       </div>
     </header>
-  )
+  );
 }
 
-export { AppHeader }
+export { AppHeader };
