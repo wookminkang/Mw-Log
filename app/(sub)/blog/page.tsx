018ab00e87fdef2c48c988e18b211f5161bbb906
kangminwook <kangmu950@gmail.com>
2025-12-31 08:45:59 +0900

    log 카테고리 추가

---
+++ app/(sub)/blog/page.tsx
@@ -0,0 +1,18 @@
+import { Separator } from "@/components/ui/separator"
+
+
+export default function BlogPage(){
+  return (
+    <div>
+      <h2>Short Diary</h2>
+      <h3>Records of Thoughts, Feelings and Lessons that are Too Short to be an Article.</h3>
+      <div className="grid grid-cols-1 gap-10">
+        <div className="blog-item ">
+
+        </div>
+
+        <Separator />
+      </div>
+    </div>
+  )
+}
--- components/common/AppHeader.tsx
+++ components/common/AppHeader.tsx
@@ -17,9 +17,14 @@ function AppHeader() {
       <div className="mx-auto max-w-[960px] px-4 py-3 flex items-center justify-between">
         <Link href="/" className="text-xl font-bold flex items-center gap-2">
           <Image src="/logo.png" alt="logo" width={18} height={18} />
-          <span>MINWOOK </span>
+          <span>MINWOOK</span>
         </Link>
-        <nav className="flex items-center gap-6">
+        <nav className="flex items-center gap-5">
+          
+          <Link href="/blog" className="border-b-3 border-orange-500 hover:text-gray-500 transition-all duration-300">
+            Log
+          </Link>
+          
           {path === "/" && (
             <Button
               variant="ghost"
