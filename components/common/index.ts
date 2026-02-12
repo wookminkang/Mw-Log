9c080b2160f7b080155359ef4c36ad99e0b9eecc
mwkang1 <mwkang1@smartscore.kr>
2026-02-12 19:49:56 +0900

    청크파일 재 분리

--- components/common/index.ts
+++ components/common/index.ts
@@ -1,7 +1,6 @@
-import { AppHeader } from "@/components/common/AppHeader"
-import { AppFooter } from "@/components/common/AppFooter"
-import { AppSidebar } from "@/components/common/AppSidebar"
-import { AppEditor } from "@/components/common/AppEditor"
-import { AdminSideBar } from "./AdminSideBar"
-
-export { AppHeader, AppFooter, AppSidebar, AppEditor, AdminSideBar }
+// 가벼운 배럴: 사용 빈도 높은 컴포넌트만 재내보내기
+// 무거운 에디터류(AppEditor 등)는 여기서 제외하여 공용 청크로 끌려가지 않게 함
+export { AppHeader } from "./AppHeader";
+export { AppFooter } from "./AppFooter";
+export { AppSidebar } from "./AppSidebar";
+export { AdminSideBar } from "./AdminSideBar";
